0bc9927a837afa6954feeb590330f89134731954
kasperobig <kassivenceslas@gmail.com>
2022-05-11 13:18:28 +0300

    master

--- Main.py
+++ Main.py
@@ -19,14 +19,14 @@ class PlayfairInter(Frame):
         self.clearword.place(x=320, y=10)
         self.input1 = Text(self)
         self.input1.config(width=30, height=15)
-        self.input1.insert('0.0', 'Oh it is scary')
+        self.input1.insert('0.0')
         self.input1.place(x=50, y=30)
         self.input2 = Text(self)
         self.input2.config(width=30, height=15)
-        self.input2.insert('0.0', 'derknabeimmoor')
+        self.input2.insert('0.0')
         self.input2.place(x=320, y=30)
         self.bStart = Button(self, text="Encrypt/Decrypt")
-        self.bStart.bind("<Button-1>", self.starten)
+        self.bStart.bind("<Button-1>", self.start)
         self.bStart.place(x=220, y=235)
         self.label = Label(self, text="Encrypted Text")
         self.label.config(bg="lightgray")
@@ -41,6 +41,16 @@ class PlayfairInter(Frame):
         self.playfair.textFormation()
         self.playfair.Alphabet()
         print(self.playfair.code())
-        #textneu = self.playfair.globall
+        #newText = self.playfair.globall
         #self.label.config(text = self.playfair.globall)
         self.inputText.insert(END, self.playfair.globall)
+
+
+def main():
+    root = Tk()
+    root.config(width=900, height=330)
+    gui = PlayfairInter(root)
+    root.mainloop()
+
+
+main()
