53adafe3113ac3501848b2682b2d9c5cae2dfbc1
kasperobig <kassivenceslas@gmail.com>
2022-05-11 12:36:45 +0300

    master

--- Playfair.py
+++ Playfair.py
@@ -6,3 +6,30 @@ class Playfair:
         codeText = ''
         condingAlphabet = ''
         self.globall = ''
+
+     def textformation(self):
+            a = str(self.plaintext)
+        a = self.plaintext.upper()
+        text = ''
+        for i in range(len(a)):
+            if a[i] == 'Ä':
+                text = text + 'AE'
+            elif a[i] == 'Ö':
+                text = text + 'OE'
+            elif a[i] == 'Ü':
+                text = text + 'UE'
+            elif a[i] == 'ß':
+                text = text + 'SZ'
+            elif a[i] == '"':
+                text = text + ''
+            elif a[i] == "'":
+                text = text + ''
+            elif a[i] == ".":
+                text = text + ''
+            elif a[i] == " ":
+                text = text + ''
+            elif a[i] == "J":
+                text = text + 'I'
+            else:
+                text = text + a[i]
+        text2 = ''
