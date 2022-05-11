82d233c082824e27fc89a2c64581d8bbfd6ee95f
kasperobig <kassivenceslas@gmail.com>
2022-05-11 12:53:31 +0300

    master

--- Main.py
+++ Main.py
@@ -3,3 +3,10 @@ from Playfair import *
 
 
 class PlayfairInter(Frame):
+
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        self.master.title("Playfair")
+        self.place(x=0, y=0)
+        self.config(width=900, height=330)
+        self.createWidgets()
